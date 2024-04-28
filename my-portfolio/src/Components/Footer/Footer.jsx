30e463923aad9b651bafd92496c665f895f87a35
Rucha Parve <ruchaparve76@gmail.com>
2024-04-28 18:54:54 +0530

    changes

---
+++ my-portfolio/src/Components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Footer.css'
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="footer-top">
+        <div className="footer-top-left">
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <div className="footer-bottom-right">
+            <p>Term of Services</p>
+            <p>Privacy Policy</p>
+            <p>Connect with me</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
